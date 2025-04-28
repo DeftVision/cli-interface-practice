3131e89e6001b67273b3d5281455adfecbf195fe
Gabriel <gjbarret@asu.edu>
2025-04-28 14:19:13 -0600

    Establishes basic backend API
    Sets up a basic Express server with a "Hello from backend!" message endpoint.
    This allows the frontend to fetch data from the backend and confirm the connection is configured correctly.
    Adds CORS to prevent any access errors

--- phase-2-frontend/src/App.jsx
+++ phase-2-frontend/src/App.jsx
@@ -5,18 +5,18 @@ function App() {
 
     const handleClick = async () => {
         try {
-            const response = await fetch('http://localhost:3000/api/message', {
+            const response = await fetch(`http://localhost:3000/api/message`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
 
-            const _response = await response.json();
+            const data = await response.json();
             if (!response.ok) {
                 console.log('whoops... there was an error')
             }
-            console.log(_response)
+            console.log('Message from the back', data.message)
 
         } catch (error) {
             console.error('looks like you hit the catch block, something\'s wrong');
